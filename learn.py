1c52dc3c0f3abf447c86375711fcd06423ea86fa
Debdeep Banerjee <debdeep.banerjee31@gmail.com>
2022-11-06 00:55:54 +0530

    changing for 3rd time

--- learn.py
+++ learn.py
@@ -1,11 +1,1 @@
-<<<<<<< HEAD
-print("lets learn git and github")
-print("Are you making changes..?")
-ch = input()
-if (ch == 1):
-    print("Okay Change made..")
-else:
-    print("No Changes made")
-=======
-print("lets learn git and github")
->>>>>>> 4f6a77d34a206b8b399aa3062f5ba9dbd74a63a9
+print("Changing agian")
